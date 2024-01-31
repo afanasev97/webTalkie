d868dfe8b4de726c4db3c883726d41abb8f1efcf
Timur Nabeev <nabeevtimur@gmail.com>
2024-01-31 21:09:45 +0300

    upd: temp cllient-serv

--- ws_client.js
+++ ws_client.js
@@ -1,51 +1,71 @@
 const { WebSocket } = require('ws');
 
-const ws = new WebSocket('ws://127.0.0.1:8031');
-const userMsg = 'Privet timur!\nKak ti?';
+const userName = 'Ilia';
+const ws = new WebSocket(`ws://127.0.0.1:8031?username=${userName}`);
 
-const userName = 'Ilia'
+const testMsg = 'Privet timur!\nKak ti?';
+
 
 ws.on('error', console.error);
 
 ws.on('open', function open() {
-	ws.send(initMsg(userName));
-	// ws.send(formatMsg(userName, userMsg, 'timur'));
+	ws.send(formatMsg(testMsg, 'Timur'));
 });
 
-ws.on('message', function message(data) {
-	console.log('received: %s', data);
-});
+ws.on('message', onMessage);
 
-
-function initMsg(userName) {
-	const initMessage = {
-		header: {
-			from: userName,
-
-		},
-		type: 'init'
-	}
-	return JSON.stringify(initMessage)
-
-
-}
-function formatMsg(userName, userMsg, destination) {
+/* outcome functions */
+function formatMsg(userMsg, destination) {
 	const formattedMsg = {
+		type: 'message',
 		header: {
-			from: userName,
-			to: destination,
-			type: 'msg',
+			destination
 		},
 		msg: userMsg.split('\n')
 	}
 	return JSON.stringify(formattedMsg);
 }
 
-function makeMsgObj(msg) {
+/* income functions */
+
+function onMessage(data) {
 	try {
-		msgObject = JSON.parse(msg);
-		return msgObject;
+		const incomeMsg = JSON.parse(data.toString());
+		processMessage(incomeMsg);
 	} catch (e) {
-		log(e)
+		log(`Error parsing incomming msg Error: ${e}`);
+		log(`data: ${data}`);
 	}
+}
+
+
+function processMessage(msg) {
+	if (!msg.type) return;
+	switch (msg.type) {
+		case 'message': handleMessage(msg.body, msg.header?.from); return;
+		case 'info': handleInfo(msg.header?.info, msg.body); return;
+		case 'error': handleError(msg.header?.error, msg.body); return;
+		default: log('Unknown message type: ' + msg.type);
+	}
+}
+
+function handleMessage(msgBody, from) {
+	if (!msgBody ||
+		!from) return;
+	const msgToShow = `${from}:\n${msgBody}`;
+	log(msgToShow);
+}
+
+function handleInfo(headerInfo, info) {
+	if (!headerInfo ||
+		!info) return;
+	const msgToShow = `${headerInfo}:\n${info}`;
+	log(msgToShow);
+}
+
+function handleError(headerError, errorBody) {
+	if (!headerError ||
+		!errorBody) return;
+	const errorToShow = `${headerError}:\n${errorBody}`
+	console.error(errorToShow);
 }
--- ws_server.js
+++ ws_server.js
@@ -1,36 +1,68 @@
+"use strict";
+
 var log = console.log;
 const { WebSocketServer } = require('ws');
-
-
 
 const wsServer = new WebSocketServer({ port: 8031 });
 const rawClients = new Map();
 const authorizedClients = new Map();
 wsServer.on('connection', onConnect);
 
-function onConnect(wsClient) {
-	clientAddress = `${wsClient._socket.remoteAddress}:${wsClient._socket.remotePort}`;
-	rawClients.set(clientAddress, wsClient)
-	wsClient.on('error', () => {
-		rawClients.delete(clientAddress);
-	});
-	wsClient.on('close', (code, reason) => {
-		log(`Client: ${req.socket.remoteAddress}:${req.socket.remotePort}`)
-		log(`disconnected with code: ${code}`)
-		if (String(reason)) log(`disconnected reason: ${String(reason)}`)
-		rawClients.delete(clientAddress);
+function onConnect(wsClient, req) {
+	const userName = req.url.slice(11);
+	if (!userName || (authorizedClients.has(userName))) {
+		wsClient.send('Username null or already used');
+		wsClient.close(); // TODO reason
+		return;
+	}
+
+	authorizedClients.set(userName, wsClient)
+
+	wsClient.on('error', (error) => {
+		log(`Error: ${error}`)
 	});
 
-	wsClient.on('message', (data) => onMessage(data, wsClient));
+	wsClient.on('close', (code, reason) => {
+		log(`Client: ${userName}`)
+		log(`disconnected with code: ${code}`)
+		if (String(reason)) log(`disconnected reason: ${String(reason)}`)
+		authorizedClients.delete(userName);
+
+		// TODO del from authorizedClients
+	});
+
+	wsClient.on('message', onMessage);
 
 
 	wsClient.send('something');
 }
 
-function onMessage(data, wsClient) {
-	let msgObj = JSON.parse(String(data));
-	let msgType = msgObj.type
-	if (msgType == 'init') {
-		authorizedClients.set(msgObj.header.from, wsClient)
+
+function onMessage(data) {
+	try {
+		const incomeMsg = JSON.parse(data.toString());
+		processMessage(incomeMsg);
+	} catch (e) {
+		log(`Error parsing incomming msg Error: ${e}`);
+		log(`data: ${data}`);
+	}
+}
+
+
+function processMessage(msg) {
+	if (!msg.type) return;
+	switch (msg.type) {
+		case 'message': handleMessage(msg.body, msg.header?.destination); return;
+		case 'info': handleInfo(msg.header?.info, msg.body); return;
+		default: log('Unknown message type: ' + msg.type);
+	}
+}
+
+function handleMessage(msgBody, destination) {
+	if (!msgBody ||
+		!destination) return
+	if (authorizedClients.has(destination)) {
+		destinationWsClient = authorizedClients.get(destination)
+		destinationWsClient.send(msgBody)
 	}
 }
